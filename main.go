f165117390c62020e324f8972d19c1909c3c0ca7
Drew Burt <drew.burt@cancer.org>
2022-06-17 10:29:05 -0400

    module 2

--- main.go
+++ main.go
@@ -18,15 +18,26 @@ var errInvalidArguments = errors.New("Invalid arguments")
 var errReadingInput = errors.New("Error reading input")
 
 func main() {
-  
-if len(os.Args) != 2 {
-	printError(errInvalidArguments)
-}
 
-originUnit = strings.ToUpper(os.Args[1])
+	if len(os.Args) != 2 {
+		printError(errInvalidArguments)
+	}
+
+	originUnit = strings.ToUpper(os.Args[1])
 
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
+		_, err := fmt.Scanln(&originValue)
+		if err != nil {
+
+			printError(errReadingInput)
+
+		}
+		if originUnit == "C" {
+			convertToFahrenheit(originValue)
+		} else {
+			convertToCelsius(originValue)
+		}
 
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
 
